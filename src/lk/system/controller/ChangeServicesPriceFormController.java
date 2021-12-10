a807fbf35fcdb2160272273f674a06d6ef307063
Jagad Chandana <jagadchandana61@gmail.com>
2021-12-10 17:49:48 +0530

    Complete UI Service Change Form(II)

--- src/lk/system/controller/ChangeServicesPriceFormController.java
+++ src/lk/system/controller/ChangeServicesPriceFormController.java
@@ -1,7 +1,9 @@
 package lk.system.controller;
 
+import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.stage.Stage;
 
 public class ChangeServicesPriceFormController {
 
@@ -18,10 +20,14 @@ public class ChangeServicesPriceFormController {
     public JFXTextField txtClassC;
     public JFXTextField txtNormal;
     public JFXTextField txtPermanent;
+    public JFXButton btnClose;
 
     public void updateOnAction(ActionEvent actionEvent) {
     }
 
     public void closeOnAction(ActionEvent actionEvent) {
+        Stage stage = (Stage) btnClose.getScene().getWindow();
+        // do what you have to do
+        stage.close();
     }
 }
--- src/lk/system/controller/ServicesFormController.java
+++ src/lk/system/controller/ServicesFormController.java
@@ -5,9 +5,15 @@ import com.jfoenix.controls.JFXCheckBox;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.stage.Stage;
 import lk.system.db.DBConnection;
+
+import java.io.IOException;
 
 public class ServicesFormController {
     public JFXTextField txtId;
@@ -33,7 +39,17 @@ public class ServicesFormController {
     public JFXButton btnSave;
 
     public void changePriceOnAction(ActionEvent actionEvent) {
-
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("../views/ChangeServicesPriceForm.fxml"));
+        Parent root = null;
+        try {
+            root = (Parent) fxmlLoader.load();
+        } catch (IOException e) {
+            System.out.println("Error");
+        }
+        Stage stage = new Stage();
+        stage.setTitle("Price Change");
+        stage.setScene(new Scene(root));
+        stage.show();
     }
 
     public void newServicesOnAction(ActionEvent actionEvent) {
@@ -45,7 +61,7 @@ public class ServicesFormController {
         String id=txtId.getText();
         String name=txtName.getText();
         String type= String.valueOf(txtType.getValue());
-        Double food=cbxFood.selectedProperty();
+      //  Double food=cbxFood.selectedProperty();
         if (btnSave.getText().equalsIgnoreCase("New Services")){
 
         }
